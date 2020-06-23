7a0e94dbb038d8a468fa74ba1f94e082d0bdf462
Andre Butler <andrebutler@mmc.school.nz>
2020-06-23 12:28:40 +1200

    First commit

---
+++ Dodge - Trial No1 - S23062020/FrmDodge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dodge___Trial_No1___S23062020
+{
+    public partial class FrmDodge : Form
+    {
+        public FrmDodge()
+        {
+            InitializeComponent();
+        }
+    }
+}
