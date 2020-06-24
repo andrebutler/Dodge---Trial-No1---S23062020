42a9f8254615ac6d74a1fe3cb33e9e002da39a81
Andre Butler <andrebutler@mmc.school.nz>
2020-06-24 14:58:25 +1200

    Tried to make images display, but was unsuccessful.

--- Dodge - Trial No1 - S23062020/FrmDodge.cs
+++ Dodge - Trial No1 - S23062020/FrmDodge.cs
@@ -12,9 +12,21 @@ namespace Dodge___Trial_No1___S23062020
 {
     public partial class FrmDodge : Form
     {
+        Graphics g; //declare a graphics object called g
+        Planet planet1 = new Planet(); //create the object, planet1
+
         public FrmDodge()
         {
             InitializeComponent();
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            //call the Planet class's DrawPlanet method to draw the image planet1 
+            planet1.DrawPlanet(g);
+
         }
     }
 }
---
+++ Dodge - Trial No1 - S23062020/Planet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Dodge___Trial_No1___S23062020
+{
+    class Planet
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image planetImage;//variable for the planet's image
+
+        public Rectangle planetRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Planet()
+        {
+            // Methods for the Planet class
+            public void DrawPlanet(Graphics g)
+            {
+                g.DrawImage(PlanetImage, PlanetRec);
+            }
+
+            x = 10;
+            y = 10;
+            width = 20;
+            height = 20;
+            //planetImage contains the plane1.png image
+            planetImage = Properties.Resources.planet1;
+            planetRec = new Rectangle(x, y, width, height);
+        }
+
+
+    }
+}
